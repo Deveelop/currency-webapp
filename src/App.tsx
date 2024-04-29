08bd978506cf7fec3a233b02e980064d89068a5e
Victor Abuka <vicsonabuka@gmail.com>
2024-04-29 17:46:17 +0100

    Added Converter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+
+import CurrencyConvert from "./components/CurrencyConvert"
 import Header from "./components/Header"
+
+
 function App() {
+
+  
   return (
     <>
 <Header/>
+  <div className="min-h-screen flex flex-col items-center justify-center">
+    <CurrencyConvert/>
+  </div>
     </>
   )
 }
