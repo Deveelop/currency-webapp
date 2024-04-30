c243d1c373152ceaaa6e126388445d65e847f36f
Victor Abuka <vicsonabuka@gmail.com>
2024-04-30 16:31:08 +0100

    Added new commits

--- src/components/CurrencyConvert.tsx
+++ src/components/CurrencyConvert.tsx
@@ -1,24 +1,74 @@
+import { ChangeEvent, useEffect, useState } from "react"
+import CurrencyDropDown from "./CurrencyDropDown"
+
 
 
 const CurrencyConvert = () => {
-    // https://api.frankfurter.app/currencies
-    // https://api.frankfurter.app/latest?amount=1&from=USD$to=INR
+   // https://api.frankfurter.app/latest?amount=1&from=USD$to=INR
+    const [currencies, setCurrencies] = useState<string[]>([])
+    const [amount, setAmount] = useState(1)
+    const [ fromCurrency, setFromCurrency] = useState('USD')
+    const [ toCurrency, setToCurrency] = useState('INR')
+
+      // https://api.frankfurter.app/currencies
+      
+
+    const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => {
+      const valueAsNumber = parseFloat(e.target.value);
+      setAmount(valueAsNumber);
+    }
+    const fetchCurrencies = async () => {
+      try{
+        const res = await fetch("https://api.frankfurter.app/currencies");
+        const data = await res.json();
+        setCurrencies(Object.keys(data));
+      }catch(error){
+        console.error('encountered error', error)
+      }
+    }
+    useEffect(() => {
+      fetchCurrencies()
+    }, [])
+
+    console.log(currencies)
+
+    const convertCurrencies = () => {
+     
+    }
+
+    const handleFavorites = (currency:React.MouseEvent<HTMLButtonElement>) => {
+      
+    }
+      
   return (
     <div className=" max-w-xl m-auto my-10 p-5 bg-white rounded-lg shadow-md">
-      <h2 className=" mb-5 text-2xl font-semibold text-gray-700">Currency Converter</h2>
-      <div>Dropdowns</div>
+      <h2 className=" mb-5 text-2xl font-semibold text-gray-700 ">Currency Converter</h2>
+      <div className=" border-b-2"/>
+      <div>
+        <CurrencyDropDown currencies={currencies}  title="From" selectedCurrency={fromCurrency}
+  setCurrency={setFromCurrency} handleFavorites={handleFavorites}/>
+        <div>
+          <button>
+          
+          </button>
+        </div>
+        <CurrencyDropDown currencies={currencies} title="To" selectedCurrency={toCurrency}
+  setCurrency={setToCurrency} handleFavorites={handleFavorites}/>
+      </div>
       <div className=" mt-4">
         <label
          htmlFor="amount"
          className=" block text-sm font-medium text-gray-700">Amount:</label>
         <input
+        value={amount}
+        onChange={onChangeHandle}
          type="number"
          className=" w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500  mt-1"
          />
       </div>
 
       <div className=" flex justify-end mt-6">
-        <button className=" px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        <button onClick={convertCurrencies} className=" px-5 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
           Convert
         </button>
       </div>
@@ -29,4 +79,4 @@ const CurrencyConvert = () => {
   )
 }
 
-export default CurrencyConvert
+export default CurrencyConvert;
---
+++ src/components/CurrencyDropDown.tsx
@@ -0,0 +1,55 @@
+
+import { HiOutlineStar } from 'react-icons/hi';
+
+
+type CurrencyDropDownProps = {
+  currencies: string[];
+  currency?: string
+  selectedCurrency: string;
+  setCurrency: (currency: string) => void;
+  favorites?: string[];
+  handleFavorites?: (currency:React.MouseEvent<HTMLButtonElement>) => void;
+  title?: string;
+}
+
+const CurrencyDropDown= ({
+  currencies,
+  currency,
+  selectedCurrency,
+  setCurrency,
+  favorites,
+  handleFavorites,
+  title = "",
+}: CurrencyDropDownProps) => {
+ 
+
+  return (
+    <div>
+      <label
+        htmlFor={title}
+        className="block text-sm font-medium text-gray-700"
+      >
+        {title}
+      </label>
+      <div className=" mt-1 relative">
+        <select
+        value={currency}
+        onChange={(e) => setCurrency(e.target.value)}
+         className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          <hr/>
+          {currencies.map((curr) => (
+            <option value={curr} key={curr}>
+              {curr}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleFavorites} className=' absolute inset-y-0 right-0 pr-5 flex items-center text-sm leading-5'>
+        <HiOutlineStar/>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyDropDown;
